4661a241e181b053c4acbb681e734725f56014c6
Violet Tan <violet_tan@hive.gov.sg>
2022-10-06 14:01:33 +0800

    commands to install mongosh

--- terraform/userdata/sdx_kube_bastion_userdata.sh
+++ terraform/userdata/sdx_kube_bastion_userdata.sh
@@ -94,14 +94,10 @@ enabled_bamboo_envvars() {
 }
 
 install_mongo() {
-  wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
-  echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
-  sudo apt-get update
-  wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4_amd64.deb
-  sudo dpkg -i ./libssl1.1_1.1.0g-2ubuntu4_amd64.deb
-  rm -i libssl1.1_1.1.0g-2ubuntu4_amd64.deb
-  sudo apt-get install -y mongodb-org
-  sudo systemctl start mongod
+  wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo apt-key add -
+  echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+  apt-get update
+  apt-get install -y mongodb-mongosh
   wget https://s3.amazonaws.com/rds-downloads/rds-combined-ca-bundle.pem
 }
 
