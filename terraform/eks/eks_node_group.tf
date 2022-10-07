037da50f365723c4097e776ce4085c6c738a8b52
Ramya Palaniappaan <ramyadevi@nsearchglobal.com>
2022-10-07 09:29:27 +0800

    node group policy update

--- terraform/eks/eks_node_group.tf
+++ terraform/eks/eks_node_group.tf
@@ -41,9 +41,9 @@ resource "aws_eks_node_group" "sdx-eks-default-nodegroup" {
   }
 
   depends_on = [
-    #aws_iam_role_policy_attachment.sdx-node-AmazonEKSWorkerNodePolicy,
-    #aws_iam_role_policy_attachment.sdx-node-AmazonEKS_CNI_Policy,
-    #aws_iam_role_policy_attachment.sdx-node-AmazonEC2ContainerRegistryReadOnly,
+    aws_iam_role_policy_attachment.sdx-node-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.sdx-node-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.sdx-node-AmazonEC2ContainerRegistryReadOnly,
     aws_iam_role_policy_attachment.sdx-node-u-eks,
   ]
 }
