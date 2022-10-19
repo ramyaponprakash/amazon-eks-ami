a976d91d540f21fb13c35942ddaf985fd7eb3910
Alex Min <alex_min@tech.gov.sg>
2022-10-19 16:43:05 +0800

    UPDATED helm chart push to check the version exist already or not

--- cicd/bamboo/base-publish-chart.sh
+++ cicd/bamboo/base-publish-chart.sh
@@ -24,9 +24,17 @@ echo "packaging $CHART_NAME"
 "${LOC}"helm package ./base/"$CHART_NAME"/ --destination "$DEST_PATH" || exit 1
 
 for file in "$DEST_PATH"/*; do
- FILE_NAME=${file##*/}
- echo "publishing $FILE_NAME to s3 $CHART_NAME"
- "${LOC}"helm s3 push "$DEST_PATH/$FILE_NAME" "$CHART_NAME" --force || exit 1
+  FILE_NAME=${file##*/}
+  VERSION_EXT=${file##*-}
+  VERSION=${VERSION_EXT%.*}
+  echo "checking version $VERSION exist..."
+  VERSION_RESULT=$(helm search repo "$CHART_NAME/$CHART_NAME" --version "$VERSION") # always exit 0
+  if [ "$VERSION_RESULT" == "No results found" ]; then
+     echo "publishing $FILE_NAME to s3 $CHART_NAME"
+     "${LOC}"helm s3 push "$DEST_PATH/$FILE_NAME" "$CHART_NAME" --force || exit 1
+  else
+     echo "$VERSION exist in $CHART_NAME/$CHART_NAME, skip to push"
+  fi
 done
 
 echo "publish $CHART_NAME - done!"
