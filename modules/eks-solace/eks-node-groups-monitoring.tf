15e73e8c94c81a554bc55de77026d37a776294ba
alexmin <alex_min@tech.gov.sg>
2023-04-21 14:35:40 +0800

    Updated for aws-loadbalancer-controller

--- modules/eks-solace/eks-node-groups-monitoring.tf
+++ modules/eks-solace/eks-node-groups-monitoring.tf
@@ -6,7 +6,7 @@ resource "aws_eks_node_group" "monitoring" {
   }
 
   node_role_arn = var.eks_node_role_arn
-  subnet_ids    = [var.eks_private_subnet_ids[length(var.eks_private_subnet_ids) - 1]]
+  subnet_ids    = [var.eks_private_subnet_ids[2]]
 
   cluster_name           = var.cluster_name
   node_group_name_prefix = "${var.cluster_name}-moni-"
@@ -38,7 +38,7 @@ resource "aws_launch_template" "monitoring" {
   name = "${var.cluster_name}-monitoring-ng-tmpl"
 
   image_id               = data.aws_ssm_parameter.optimized-ami.value
-  vpc_security_group_ids = local.cluster_secgrp_ids
+  vpc_security_group_ids = var.cluster_node_secgrp_id
 
   instance_type          = var.node_groups_monitoring_instance_type
   update_default_version = true
--- modules/eks/eks-storage-class.tf
+++ modules/eks/eks-storage-class.tf
@@ -24,7 +24,6 @@ resource "kubernetes_storage_class" "gp2" {
     type      = "gp3"
     fsType    = "xfs"
     encrypted = "true"
-    kmsKeyId  = var.eks_customer_cmk_key_arn
   }
   allow_volume_expansion = true
   volume_binding_mode    = "WaitForFirstConsumer"
@@ -44,7 +43,6 @@ resource "kubernetes_storage_class" "gp3" {
     type      = "gp3"
     fsType    = "xfs"
     encrypted = "true"
-    kmsKeyId  = var.eks_customer_cmk_key_arn
   }
   allow_volume_expansion = true
   volume_binding_mode    = "WaitForFirstConsumer"
