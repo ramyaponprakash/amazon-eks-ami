45e92592b4cb547c2c4e4b9903518742a5db632a
alexmin <alex_min@tech.gov.sg>
2023-04-24 15:31:55 +0800

    ADDED cloud agent install script

--- cicd/sol-deploy-cloud-agent.sh
+++ cicd/sol-deploy-cloud-agent.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+MCA_VERSION=$1
+
+if [[ -z $MCA_VERSION ]]; then
+  MCA_VERSION="1.12.0"
+fi
+
+helm upgrade -install cloud-agent-rel https://cloud-agent-helm.s3.eu-north-1.amazonaws.com/solace-cloud-ca-$MCA_VERSION.tgz --namespace solace-cloud --values values.yaml
