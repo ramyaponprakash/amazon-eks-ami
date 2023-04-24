83c996ee52da42ad287f470cbf5ee6cb7cd48204
Ramya <ramya.palaniappan@nsearchglobal.com>
2023-04-24 10:36:45 +0800

    bastion cidr variable update

--- environments/sol-poc-private/variable.tf
+++ environments/sol-poc-private/variable.tf
@@ -191,13 +191,7 @@ variable "bastion_secgrp_ingress_cidr" {
     description = string
   }))
 
-  default = [
-    {
-      cidrs       = []
-      port        = 22
-      description = "from own vpc"
-    },
-  ]
+  default = []
 }
 
 variable "bastion_secgrp_ingress_prefix_list" {
--- environments/sol-prod/variable.tf
+++ environments/sol-prod/variable.tf
@@ -201,13 +201,7 @@ variable "bastion_secgrp_ingress_cidr" {
     description = string
   }))
 
-  default = [
-    {
-      cidrs       = []
-      port        = 22
-      description = "from own vpc"
-    },
-  ]
+  default = []
 }
 
 variable "bastion_secgrp_ingress_prefix_list" {
