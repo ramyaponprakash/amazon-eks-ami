5568ebd46ff81c37afdbe8fde53c25dfb0d57044
Ramya <ramyadevi@nsearchglobal.com>
2024-05-24 14:37:27 +0800

    updated sec grp rule for HA

--- modules/eks/eks-workers-security-groups.tf
+++ modules/eks/eks-workers-security-groups.tf
@@ -73,7 +73,7 @@ resource "aws_security_group_rule" "eks_cluster-node-egress" {
 }
 
 resource "aws_security_group_rule" "eks_cluster-node-egress-tcp-HA" {
-  for_each                 = { for index, obj in var.eks_worker_node_egress_tcp_HA : index => obj }
+  cidr_blocks              = var.eks_worker_node_egress_tcp_HA
   from_port                = 8300
   protocol                 = "tcp"
   security_group_id        = aws_security_group.eks_cluster-node.id
@@ -82,7 +82,7 @@ resource "aws_security_group_rule" "eks_cluster-node-egress-tcp-HA" {
 }
 
 resource "aws_security_group_rule" "eks_cluster-node-egress-udp-HA" {
-  for_each                 = { for index, obj in var.eks_worker_node_egress_udp_HA : index => obj }
+  cidr_blocks              = var.eks_worker_node_egress_udp_HA
   from_port                = 8301
   protocol                 = "udp"
   security_group_id        = aws_security_group.eks_cluster-node.id
