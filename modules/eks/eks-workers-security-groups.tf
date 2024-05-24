73dec1ee8e257cbce3c10379f4ab4015f5d7b38d
Ramya <ramyadevi@nsearchglobal.com>
2024-05-24 16:21:28 +0800

    egress update for solace intra

--- modules/eks/eks-workers-security-groups.tf
+++ modules/eks/eks-workers-security-groups.tf
@@ -105,7 +105,7 @@ resource "aws_security_group_rule" "eks_cluster-node-egress-udp-dns" {
   type                     = "egress"
   security_group_id        = aws_security_group.eks_cluster-node.id
   source_security_group_id = aws_security_group.eks_cluster-node.id
-  protocol                 = "tcp"
+  protocol                 = "udp"
   from_port                = each.value.from_port
   to_port                  = each.value.to_port
 }
