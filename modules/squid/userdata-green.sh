4f0379409be1818955266eb6cbee9f529a6353a0
Ramya <ramyadevi@nsearchglobal.com>
2024-09-23 13:40:23 +0800

    update squid userdata

--- modules/squid/userdata-green.sh
+++ modules/squid/userdata-green.sh
@@ -1,5 +1,34 @@
 #!/bin/bash
 
+setup_proxy() {
+  # Skip update_env_vars task if http_proxy is "empty"
+  if [ "${http_proxy}" == "" ]; then
+    echo "Skipping update_env_vars task because http_proxy is empty"
+    return
+  fi
+
+   # Write the environment variables to the temporary file
+  cat <<EOF > /etc/environment
+http_proxy=${http_proxy}
+https_proxy=${https_proxy}
+no_proxy=${no_proxy}
+EOF
+  chmod 644 /etc/environment
+
+  if [ "$PKG" == "apt" ]; then
+    cat << EOF > /etc/apt/apt.conf.d/proxy.conf
+Acquire::http::Proxy "${http_proxy}";
+Acquire::https::Proxy "${https_proxy}";
+EOF
+  elif [ "$PKG" == "yum" ]; then
+    cat << EOL >> /etc/yum.conf
+proxy=${https_proxy}
+proxy_username=
+proxy_password=
+EOL
+  fi
+}
+
 yum_update() {
     yum update -y
     curl -s https://adex-lib-mirror.s3.ap-southeast-1.amazonaws.com/aws/awscliv2.tgz -o ~/awscliv2.tgz
@@ -30,6 +59,7 @@ install_squid() {
     echo "proxy=http://127.0.0.1:3128" >> /etc/yum.conf
     export http_proxy=http://127.0.0.1:3128
     export https_proxy=http://127.0.0.1:3128
+    systemctl disable rsyslog
 }
 install_squid
 
